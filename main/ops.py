55f8984c7d1a31515d0bc52142be5a9bd406712a
yogson <elukianov@isgneuro.com>
2021-06-30 23:05:12 +0300

    ref: mqtt module back

--- main/ops.py
+++ main/ops.py
@@ -43,4 +43,4 @@ class Comm:
 
 
 mqtt = Mqtt('syrabond_automation_' + str(uuid()), clean_session=False, handler=MessageHandler())
-#mqtt_sender = Mqtt('syrabond_server_' + str(uuid()), clean_session=False, handler=MessageHandler())
+mqtt_sender = Mqtt('syrabond_server_' + str(uuid()), clean_session=True)
